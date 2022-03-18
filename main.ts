f365b9b10047b8e3da04cf7e3ea792434e5e1974
TreyR34 <101876551+TreyR34@users.noreply.github.com>
2022-03-18 09:53:00 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,31 @@
+function court (bool: boolean) {
+    if (!(coach >= 18)) {
+    	
+    } else {
+    	
+    }
+}
+let defense: Sprite = null
+let coach = 0
 game.splash("Welcome Coach!")
-let coach = game.askForNumber("How old are you?", 2)
+coach = game.askForNumber("How old are you?", 2)
+game.onUpdateInterval(5000, function () {
+    defense = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, 50, 50)
+})
