d2a0c12e6846834e98368f222caa5ead533ada33
jumbojj83 <101876529+jumbojj83@users.noreply.github.com>
2022-03-21 08:56:46 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,25 @@ let defense: Sprite = null
 let coach = 0
 game.splash("Welcome Coach!")
 coach = game.askForNumber("How old are you?", 2)
+let basketball = darts.create(img`
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    4 4 4 4 f 4 4 4 4 4 f 4 4 4 4 4 
+    4 4 . f f f . f . . f . f . 4 4 
+    4 4 . f . f . f . f f . f . 4 4 
+    4 4 1 1 . f . f . f . . f . 4 4 
+    4 4 . 1 1 f 1 f . f . . f . 4 4 
+    4 4 . f . f f 1 f f 1 1 1 1 1 4 
+    4 4 . f . . f 1 f 1 . . f . 4 4 
+    4 4 1 1 1 1 f 1 f 1 1 1 1 1 4 4 
+    4 4 . f . 1 1 f . . 1 . f . 4 4 
+    4 4 . f 1 1 . f f . . 1 f . 4 4 
+    4 4 . 1 1 . f f f f . 1 1 . 4 4 
+    4 4 . 1 . f f f . f f . 1 1 4 4 
+    4 4 1 f f f . f . . f f f 1 4 4 
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    `, SpriteKind.Player)
+basketball.setTrace()
 game.onUpdateInterval(5000, function () {
     defense = sprites.createProjectileFromSide(img`
         . . . . . . . . . . . . . . . . 
