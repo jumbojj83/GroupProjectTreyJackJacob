a06abd6d1a7a30042262a0a41dc1670bef9e9c4c
swartley <101876399+swartley@users.noreply.github.com>
2022-03-21 09:37:48 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,22 +14,23 @@ let coach = 0
 game.splash("Welcome Coach!")
 coach = game.askForNumber("How old are you?", 2)
 basketball = darts.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f f . . . . . 
+    . . . f f 5 5 5 5 e 5 5 f f . . . 
+    . . f 5 5 5 4 4 4 e 4 4 4 4 f . . 
+    . f e e e e e 4 4 e e e e e e f . 
+    . f 4 4 4 4 e e e e e 4 4 4 4 f . 
+    f 4 4 4 4 e e e e 4 4 e 4 4 4 2 f 
+    f 4 4 4 e e 4 4 e 4 4 4 e 4 2 2 f 
+    f 4 4 e 4 4 4 4 e e 4 4 4 e e 2 f 
+    e e e e e 4 4 4 4 e 4 4 4 2 2 e 2 
+    f 4 e 4 e e 4 4 4 e e 4 2 2 2 2 f 
+    f e e 4 4 e 4 4 4 4 e 4 2 2 2 2 f 
+    f e 4 4 4 e e 4 4 2 e 2 2 2 2 2 f 
+    . f 4 4 4 2 e 2 2 2 e 2 2 2 2 f . 
+    . f 4 4 2 2 e 2 2 2 2 e 2 2 2 f . 
+    . . f 2 2 2 e 2 2 2 2 e 2 2 f . . 
+    . . . f f 2 e e 2 2 2 e f f . . . 
+    . . . . . f f f f f f f . . . . . 
     `, SpriteKind.Player)
 basketball.setTrace()
 basketball.controlWithArrowKeys()
